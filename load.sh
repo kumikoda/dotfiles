2eef3b189e1a7f0159ebfb8f8361937cee31632e
Anson Chu <anson@uber.com>
2015-01-21 17:58:44 +0800

    added pathogen

--- load.sh
+++ load.sh
@@ -2,5 +2,13 @@
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
+
+# Vim
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 ln -s $DIR/.vimrc ~/.vimrc
+
+
+# Tmux
 ln -s $DIR/.tmux.conf ~/.tmux.conf
+tmux source-file ~/.tmux.conf
